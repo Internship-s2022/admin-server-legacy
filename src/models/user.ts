ed6e3c07826e90976c301945e7166cfd343058bf
LucianoAlarcon1 <luciano.alarcon@radiumrocket.com>
2022-09-23 17:28:15 -0300

    RA-40: created user schema

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,16 +1,47 @@
-const userSchema = {
-  firstName: {
+import { InferSchemaType, model, Schema } from 'mongoose';
+
+enum AccesRoleType {
+  MANAGER = 'Manager',
+  SUPERADMIN = 'SuperAdmin',
+  ADMIN = 'Admin',
+  EMPLOYEE = 'Employee',
+}
+
+const userSchema = new Schema({
+  firebaseUid: {
+    type: String,
     required: true,
   },
-  lastName: {
+  accessRoleType: {
+    type: AccesRoleType,
     required: true,
   },
   email: {
+    type: String,
     required: true,
   },
-  password: {
+  firstName: {
+    type: String,
     required: true,
   },
-};
+  lastName: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  workedHour: {
+    type: Number,
+    required: false,
+  },
+  isActive: {
+    type: Boolean,
+    required: true,
+  },
+});
 
-export default userSchema;
+export type User = InferSchemaType<typeof userSchema>;
+
+export default model('User', userSchema);
