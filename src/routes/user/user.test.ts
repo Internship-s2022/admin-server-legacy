1d3e3f147df6889b873433429c6da013b679a9ef
LucianoAlarcon1 <luciano.alarcon@radiumrocket.com>
2022-09-27 09:30:28 -0300

    RA-44: fixed errors and add tests for delete method

--- src/routes/user/user.test.ts
+++ src/routes/user/user.test.ts
@@ -2,11 +2,14 @@ import request from 'supertest';
 
 import app from 'src/app';
 
-describe('GET /user', () => {
-  test('It should get the user list', async () => {
-    const response = await request(app).get('/user');
-    expect(response.body.message).toBe('Showing Users.');
+//TODO GET tests
+
+const deleteIdUser = '20';
+describe('PUT - change isActive = false', () => {
+  test('It should delete user', async () => {
+    const response = await request(app).put(`/user/${deleteIdUser}`);
+    expect(response.body.message).toBe('User deleted');
     expect(response.statusCode).toBe(200);
-    expect(response.body.data.length).toBe(2);
+    expect(response.error).toBe(false);
   });
 });
