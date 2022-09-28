0f3868a600453956d9e7e16e1519792b57cced23
Paula Rinaldi <paula.rinaldi@radiumrocket.com>
2022-09-28 12:51:59 -0300

    RA-41: Added filter to getAll controller, created type interfaces for responses

---
+++ src/interfaces/index.ts
@@ -0,0 +1,15 @@
+export interface BodyResponse<T> {
+  message: string;
+  data?: T;
+  error: boolean;
+}
+export interface UserData {
+  firebaseUid: string;
+  accessRoleType: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+  location: string;
+  workedHour?: number;
+  isActive: boolean;
+}
--- src/routes/user/controllers.ts
+++ src/routes/user/controllers.ts
@@ -1,16 +1,26 @@
 import { Request, Response } from 'express';
 
-import users from '../../data/users';
+import { BodyResponse, UserData } from 'src/interfaces';
+
 import UserModel from '../../models/user';
 
-const getAllUsers = async (req: Request, res: Response) => {
+const getAllUsers = async (req: Request, res: Response<BodyResponse<UserData[]>>) => {
   try {
-    const allUsers = await UserModel.find({});
-    return res.status(200).json({
-      message: 'The list has been successfully retrieved',
-      data: allUsers,
-      error: false,
-    });
+    const allUsers = await UserModel.find(req.body);
+
+    if (allUsers.length) {
+      return res.status(200).json({
+        message: 'The list has been successfully retrieved',
+        data: allUsers,
+        error: false,
+      });
+    } else {
+      return res.status(404).json({
+        message: 'Cannot show the list of Users.',
+        data: undefined,
+        error: true,
+      });
+    }
   } catch (error: any) {
     return res.json({
       message: 'Error',
@@ -20,10 +30,10 @@ const getAllUsers = async (req: Request, res: Response) => {
   }
 };
 
-const getUserById = async (req: Request, res: Response) => {
+const getUserById = async (req: Request, res: Response<BodyResponse<UserData>>) => {
   try {
-    if (req.params.id) {
-      const userId = await UserModel.findById(req.params.id);
+    const userId = await UserModel.findById(req.params.id);
+    if (userId) {
       return res.status(200).json({
         message: `User with ID ${req.params.id} has been found`,
         data: userId,
