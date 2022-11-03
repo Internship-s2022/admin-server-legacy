4d941be8009e21e80bbc135d827ceaeaba25c92a
JuanMornik <juancruz.moreira@radiumrocket.com>
2022-11-03 18:25:39 -0300

    RA-143: tests fixed

--- src/helper/firebase/index.ts
+++ src/helper/firebase/index.ts
@@ -3,13 +3,18 @@ import admin from 'firebase-admin';
 
 config();
 
-const firebaseApp = admin.initializeApp({
-  credential: admin.credential.cert({
-    privateKey:
-      process.env.FIREBASE_PRIVATE_KEY && process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-  }),
-});
+const firebaseApp = admin.initializeApp(
+  !process.env.IS_TEST
+    ? {
+        credential: admin.credential.cert({
+          privateKey:
+            process.env.FIREBASE_PRIVATE_KEY &&
+            process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+          clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+          projectId: process.env.FIREBASE_PROJECT_ID,
+        }),
+      }
+    : undefined,
+);
 
 export default firebaseApp;
