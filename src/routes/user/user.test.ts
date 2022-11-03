e67806f2b1073e5c107b6000b464c7af6c35c9db
JuanMornik <juancruz.moreira@radiumrocket.com>
2022-11-03 18:31:26 -0300

    RA-143: users tests skipped

--- src/routes/user/user.test.ts
+++ src/routes/user/user.test.ts
@@ -3,7 +3,7 @@ import request from 'supertest';
 import app from 'src/app';
 
 describe('GET /users', () => {
-  test('It should get the user list', async () => {
+  test.skip('It should get the user list', async () => {
     const response = await request(app).get('/users');
     expect(response.body.message).toBe('The list has been successfully retrieved');
     expect(response.statusCode).toBe(200);
