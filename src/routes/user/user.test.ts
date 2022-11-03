ac6a4de203702a78f91bf832075df4080691ea99
JuanMornik <juancruz.moreira@radiumrocket.com>
2022-11-03 12:36:45 -0300

    RA-143: jest config for env vars reading

--- src/routes/user/user.test.ts
+++ src/routes/user/user.test.ts
@@ -3,10 +3,10 @@ import request from 'supertest';
 import app from 'src/app';
 
 describe('GET /users', () => {
-  test.skip('It should get the user list', async () => {
-    const response = await request(app).get('/user');
+  test('It should get the user list', async () => {
+    const response = await request(app).get('/users');
     expect(response.body.message).toBe('The list has been successfully retrieved');
     expect(response.statusCode).toBe(200);
-    expect(response.body.data.length).toBe(2);
+    expect(response.body.data.length).toBe(20);
   });
 });
