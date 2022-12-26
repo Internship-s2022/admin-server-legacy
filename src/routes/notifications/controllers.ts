5345b5ea59ed5a97dfc87419e37d1e0115296b6b
LucianoAlarcon1 <luciano.alarcon@radiumrocket.com>
2022-12-26 10:26:49 -0300

    RA-XX: Changed validation in controller

--- src/routes/notifications/controllers.ts
+++ src/routes/notifications/controllers.ts
@@ -38,19 +38,18 @@ const getAllNotifications = async (
         },
       })
       .populate('client', ['name', 'clientContact', 'localContact isActive']);
-    if (allNotifications.length === 0) {
-      return res.status(200).json({
-        message: 'The list has been successfully retrieved, but is empty',
-        data: allNotifications,
-        error: false,
-      });
-    } else {
+    if (!allNotifications.length) {
       return res.status(200).json({
         message: 'The list has been successfully retrieved',
         data: allNotifications,
         error: false,
       });
     }
+    return res.status(200).json({
+      message: 'The list has been successfully retrieved, but is empty',
+      data: allNotifications,
+      error: false,
+    });
   } catch (error: any) {
     return res.status(400).json({
       message: 'Error',
