0be5f7e3b3190ff6586b665ff48c4c87bcc5e042
LucianoAlarcon1 <luciano.alarcon@radiumrocket.com>
2023-01-09 09:20:50 -0300

    RA-328: Added validation on project name

--- src/routes/project/validations.ts
+++ src/routes/project/validations.ts
@@ -5,10 +5,11 @@ import { CriticalType, ProjectType } from './types';
 
 const createProject = (req: Request, res: Response, next: NextFunction) => {
   const schema = Joi.object({
-    projectName: Joi.string().min(3).max(35).trim().required().messages({
+    projectName: Joi.string().min(3).max(35).alphanum().trim().required().messages({
       'any.required': 'El nombre de proyecto es requerido',
       'string.min': 'El nombre debe contener al menos 3 caracteres',
       'string.max': 'El nombre no debe contener más de 35 caracteres',
+      'string.alphanum': 'El nombre debe contener solo letras y numeros',
     }),
 
     description: Joi.string().max(100).allow('').messages({
@@ -54,9 +55,10 @@ const createProject = (req: Request, res: Response, next: NextFunction) => {
 
 const editProject = (req: Request, res: Response, next: NextFunction) => {
   const schema = Joi.object({
-    projectName: Joi.string().min(3).max(35).trim().messages({
+    projectName: Joi.string().min(3).max(35).alphanum().trim().messages({
       'string.min': 'El nombre debe contener al menos 3 caracteres',
       'string.max': 'El nombre no debe contener más de 35 caracteres',
+      'string.alphanum': 'El nombre debe contener solo letras y numeros',
     }),
 
     description: Joi.string()
